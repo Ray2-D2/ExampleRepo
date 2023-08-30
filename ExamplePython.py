d5893914be832e4560bfeaf804018564f32c7e0a
Ray2-D2 <Ray2-D2@users.noreply.github.com>
2023-08-30 14:01:27 -0700

    Update ExamplePython.py
    Password Deleted!

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,5 +1,3 @@
-def test_passwords(x):
-    print(x)
 
 a = "string 1"
 b = "string 2"
@@ -15,6 +13,3 @@ job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 
 for name, job in job_list.items():
     print(f"{name} is a {job}")
-
-Password = 'SuperSecretAdminPass'
-test_passwords(Password)
